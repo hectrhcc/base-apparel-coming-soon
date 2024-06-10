3fb12c2c226405e4e9f94dacc48cbff71a68c8bd
Hector <hcontrerascorvacho@gmail.com>
2024-06-10 17:27:36 -0400

    movil without colors

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "Desaturated-Red": "hsl(0, 36%, 70%)",
+        "Soft-Red": "hsl(0, 93%, 68%)",
+        "Dark-Grayish-Red": "hsl(0, 6%, 24%)",
+        "Gradient-Text-1": "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+        "Gradient-Text-2": "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))"
+      },
+      fontFamily:{
+        Josefin: ['Josefin_Sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
