a15ed05386241257534dab6a6546945e544e7d2c
Hector <hcontrerascorvacho@gmail.com>
2024-06-10 21:40:22 -0400

    avance

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
         "Desaturated-Red": "hsl(0, 36%, 70%)",
         "Soft-Red": "hsl(0, 93%, 68%)",
         "Dark-Grayish-Red": "hsl(0, 6%, 24%)",
+       
+      },
+      backgroundImage: {
         "Gradient-Text-1": "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
         "Gradient-Text-2": "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))"
-      },
-      fontFamily:{
-        Josefin: ['Josefin_Sans']
       }
     },
   },
